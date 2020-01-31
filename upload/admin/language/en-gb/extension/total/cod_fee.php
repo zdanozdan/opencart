1bfde0f0dd53bb6981fd9f164079fcfa8709175d
zdanozdan <tomasz.zdanowski@mikran.com>
2020-01-31 18:28:02 +0100

    MikranCart lang

---
+++ upload/admin/language/en-gb/extension/total/cod_fee.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']    = 'Cash On Delivery Fee';
+
+// Text
+$_['text_extension']   = 'Order Totals';
+$_['text_success']     = 'Success: You have modified cash on delivery fee total!';
+$_['text_edit']        = 'Edit Cash on Delivery Fee Total';
+$_['text_percentage']  = 'Percentage';
+$_['text_fixed']       = 'Fixed Price';
+
+// Entry
+$_['entry_total']      = 'Order Total';
+$_['entry_fee_type']   = 'Fee Type';
+$_['entry_fee']        = 'Fee';
+$_['entry_tax_class']  = 'Tax Class';
+$_['entry_status']     = 'Status';
+$_['entry_sort_order'] = 'Sort Order';
+
+// Help
+$_['help_fee']			= 'If you use the percentage as fee type then fee amount must be less than 100!';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify cash on delivery fee total!';
+$_['error_fee_amount'] = 'You select Fee Type as Percentage, So Cash on Delivery Amount not more than 100!';

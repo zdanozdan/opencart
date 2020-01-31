7a197a3c1381a4e75c471a1e9f0020ca309643e1
zdanozdan <tomasz.zdanowski@mikran.com>
2020-01-31 15:50:54 +0100

    MikranCart fatal error

---
+++ upload/catalog/controller/error/fatal.php
@@ -0,0 +1,48 @@
+<?php
+class ControllerErrorFatal extends Controller {
+	public function index() {
+		$this->load->language('error/fatal');
+
+		$this->document->setTitle($this->language->get('heading_title'));
+
+		$data['breadcrumbs'] = array();
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('common/home')
+		);
+
+		if (isset($this->request->get['route'])) {
+			$url_data = $this->request->get;
+
+			unset($url_data['_route_']);
+
+			$route = $url_data['route'];
+
+			unset($url_data['route']);
+
+			$url = '';
+
+			if ($url_data) {
+				$url = '&' . urldecode(http_build_query($url_data, '', '&'));
+			}
+
+			$data['breadcrumbs'][] = array(
+				'text' => $this->language->get('heading_title'),
+				'href' => $this->url->link($route, $url, $this->request->server['HTTPS'])
+			);
+		}
+
+		$data['continue'] = $this->url->link('common/home');
+
+		$data['column_left'] = $this->load->controller('common/column_left');
+		$data['column_right'] = $this->load->controller('common/column_right');
+		$data['content_bottom'] = $this->load->controller('common/content_bottom');
+		$data['footer'] = $this->load->controller('common/footer');
+		$data['header'] = $this->load->controller('common/header');
+
+		//$this->response->addHeader($this->request->server['SERVER_PROTOCOL'] . ' 500 Internal Server Error');
+
+		$this->response->setOutput($this->load->view('error/fatal', $data));
+	}
+}

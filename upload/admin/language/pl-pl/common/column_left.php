2a8b76175632ee00f4dc600c4dbe8764846c2fd1
zdanozdan <tomasz.zdanowski@mikran.com>
2020-01-31 18:26:18 +0100

    MikranCart lang

---
+++ upload/admin/language/pl-pl/common/column_left.php
@@ -0,0 +1,4 @@
+<?php
+$_['text_dashboard'] = 'Konsola';
+$_['text_catalog'] = 'Katalog';
+$_['text_extension'] = 'Rozszerzenia';
---
+++ upload/admin/language/pl-pl/extension/shipping/gls.php
@@ -0,0 +1,12 @@
+<?php
+$_['heading_title'] = 'Wysyłka GLS';
+$_['text_extension'] = 'Rozszerzenia';
+$_['text_success'] = 'Super: Zmieniłeś stawkę wysyłki GLS';
+$_['text_edit'] = 'Edycja: Stawka GLS';
+$_['text_gls_shipping'] = 'Wysyłka GLS dla kraju';
+$_['entry_cost'] = 'Koszt';
+$_['entry_tax_class'] = 'Klasa podatku';
+$_['entry_geo_zone'] = 'Strefa Geo';
+$_['entry_tax'] = 'Klasa podatku';
+$_['entry_price'] = 'Cena wysyłki';
+$_['free_from'] = 'Darmowa wysyłka';
